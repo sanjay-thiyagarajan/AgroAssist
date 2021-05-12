fa1d6a2fd4acc2f76656278373560affe3787da5
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-05-12 12:24:28 +0530

    Trying to fix

--- AgroAssist/wsgi.py
+++ AgroAssist/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AgroAssist.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AgroAssist.settings.base')
 
 application = get_wsgi_application()
